6aa2e3454f5afc44d1a52e253d018f75a3585e27
OriCacsire <ocacsiregeronimo@udesa.edu.ar>
2023-05-25 17:50:28 -0300

    Se agregaron las 3 listas con los 5 elementos que se requerian. Preguntar, acerca de las listas, dado que se creo por medio de secciones y articles. Falta ver si es posible hacer la repeticion de las diferentes secciones con un for.

--- js/home.js
+++ js/home.js
@@ -1,18 +1,26 @@
-function formulario() {
-    let buscador = document.querySelector('#search-input').value;
-    if (buscador== "") {
-        alert ("por favor ingrese un espacio");
-        return false
-        
-    }
-    if (buscador.length < 3 ) {
-        alert("el termino es muy corto");
-        return false
-        
-    }
+//manipulacion
+//HOME repeticion de secciones y obtencion de informacion de las apis
+//REPETICIONES DE CANCIONES-PLAYLIST-ARTISTAS CON FOR FALTA
 
-    return true
-    
-}
+//Declaro varables para el form 
+let buscadorForm = document.querySelector("#buscadorForm");
+let searchInput = document.querySelector("#searchInput");
+
+//Agregamos un evento al buscador para que no deje buscar con campo vacio o menos de 3 carácteres
+buscadorForm.addEventListener ("submit", function(e){
+    e.preventDefault();
+    if(searchInput.value == ''){
+        alert('Debe ingresar alguna palabra');
+   } else if (searchInput.value.length < 3){
+        alert('Ingresar al menos 3 caracteres');
+   } else{
+        this.submit();
+   }
+})
+
+//guardar lo que ingresa el usuario
+searchInput.addEventListener('keydown', function(e) {
+    console.log(searchInput.value);
+})
 
 //Diseños Home
