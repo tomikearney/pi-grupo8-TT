ad6be249bca00ecb443c07226420ba14824e0061
OriCacsire <ocacsiregeronimo@udesa.edu.ar>
2023-05-25 19:02:37 -0300

    agregue un boton con un evento en la playlist, por lo pedido en la Script de la fecha 26/05/2023

---
+++ js/playlist.js
@@ -0,0 +1,15 @@
+// let apiKey = 
+//formulario de busqueda
+
+//Botón para agregar a favoritos
+let btnFavoritos = document.querySelector(".btnFavoritos");
+btnFavoritos.addEventListener('click', function(){
+    alert('Rediriendote a tu canción favorita')
+});
+
+//obtengo los datos necesarios para poder iterar, en ese caso son los favoritos.
+// let recuperoStorage = localStorage.getItem('playlist');
+// //como nos da un string, lo vuelvo a su modo original para iterarlos.
+// let playlist = JSON.parse(recuperoStorage)
+
+// console.log(playlist)
