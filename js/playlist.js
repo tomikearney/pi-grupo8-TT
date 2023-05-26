bc01d442bc298963ab5af636ffa14be97c67bcdf
frandelyerro <fdelyerro@udesa.edu.ar>
2023-05-26 20:04:51 -0300

    hice el for del favorites

---
+++ js/playlist.js
@@ -0,0 +1,11 @@
+let cancionesFavoritas = localStorage.getItem("cancionesFavoritas");
+let resultadolocal = JSON.parse(cancionesFavoritas);
+console.log(resultadolocal);
+let listacanciones= document.querySelector("articleMain");
+
+for (let i= 0; i< resultadolocal.length; i++) {
+   listacanciones.innerHTML += `<img class="song" src="./img/artistasHome.png "  alt="" >
+   <h3 class="title">Titulo</h3>
+   <p class="name">Nombre del autor</p>`
+    
+}
