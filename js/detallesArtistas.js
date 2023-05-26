ca7d5a7cf1442acedaf7d8d0ca29ff9c3ba0014c
tomikearney <tomikearney@hotmail.com>
2023-05-26 17:17:57 -0300

    corregí el js de detalles artistas para que las canciones favoritas se guarden en un array en el localStorage

--- js/detallesArtistas.js
+++ js/detallesArtistas.js
@@ -12,15 +12,28 @@ let nombreDisco = document.querySelector(".nombreDisco")
 let agregarAMiPlaylist = document.querySelector(".agregarAMiPlaylist")
 
 agregarAMiPlaylist.addEventListener("click", function(e) {
+    let cancionesFavoritas = localStorage.getItem("cancionesFavoritas");
+    
+    if (!cancionesFavoritas) {
+        cancionesFavoritas = [];
+    } else {
+        cancionesFavoritas = JSON.parse(cancionesFavoritas);
+    }
+
     if (agregarAMiPlaylist.innerHTML == "Agregar a mi playlist") {
         agregarAMiPlaylist.innerText = "Agregado a tu playlist"
-        localStorage.setItem("cancionesFavoritas", nombreCanción.innerHTML)
-        console.log(localStorage);
+
+        cancionesFavoritas.push(nombreCanción.innerHTML)
+        let cancionesFavoritasString = JSON.stringify(cancionesFavoritas)
+        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString)
 
     } else if (agregarAMiPlaylist.innerHTML == "Agregado a tu playlist"){
         agregarAMiPlaylist.innerText = "Agregar a mi playlist"
-        localStorage.removeItem(nombreCanción.innerHTML)
-        console.log(localStorage);
+
+        let indiceCanción = cancionesFavoritas.indexOf(nombreCanción.innerHTML) //Te da el índice de la canción que le pases en el array
+        cancionesFavoritas.splice(indiceCanción, 1) //Elimina la canción correspondiente a ese índice del array obtenido anteriormente
+        let cancionesFavoritasString = JSON.stringify(cancionesFavoritas)
+        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString)
     }
 })
 
