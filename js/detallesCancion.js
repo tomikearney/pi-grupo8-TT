e5dd6611b39d55a522d21f3f17cafd898e7ccd8a
tomikearney <tomikearney@hotmail.com>
2023-06-09 17:47:50 -0300

    Terminé la funcionalidad para agregar y remover canciones de favoritos(almacenadas en local storage)

--- js/detallesCancion.js
+++ js/detallesCancion.js
@@ -50,32 +50,34 @@ fetch(url)
 
 //EVENTO PARA AGREGAR Y REMOVER CANCIÓN DE FAVORITOS
 
-let agregarAMiPlaylist = document.querySelector(".agregarAMiPlaylist")
-let nombreCancion = document.querySelector(".nombreCancion")
+let agregarAMiPlaylist = document.querySelector(".agregarAMiPlaylist") //Usuario clickeará en este elemento para agregar o quitar de la playlist
+
+let cancionesFavoritas = localStorage.getItem("cancionesFavoritas");
+    
+if (!cancionesFavoritas) { //si cancionesFavoritas no existe
+    cancionesFavoritas = []; //lo crea vacío
+} else { //si cancionesFavoritas ya existe
+    cancionesFavoritas = JSON.parse(cancionesFavoritas); //lo transforma a formato manipulable en js para que lo usemos
+}
+
+if (cancionesFavoritas.includes(id)) { //si el id está en el cancionesFavoritas
+    agregarAMiPlaylist.innerText = "Quitar de tu playlist"
+}
 
 agregarAMiPlaylist.addEventListener("click", function(e) {
-    let cancionesFavoritas = localStorage.getItem("cancionesFavoritas");
-    
-    if (!cancionesFavoritas) { //si cancionesFavoritas no existe
-        cancionesFavoritas = []; //lo crea vacío
-    } else { //si cancionesFavoritas ya existe
-        cancionesFavoritas = JSON.parse(cancionesFavoritas); //lo transforma a formato manipulable para que lo usemos
-    }
-
-    if (agregarAMiPlaylist.innerHTML == "Agregar a mi playlist") { //si no se agrego a la playlist
-        agregarAMiPlaylist.innerText = "Agregado a tu playlist" 
-
-        cancionesFavoritas.push(nombreCancion.innerHTML) //Agrega la canción al array
-        let cancionesFavoritasString = JSON.stringify(cancionesFavoritas) //transforma al array a stringyfy
-        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString) //vuelve a subir el array(stringyfy) al local storage
-
-    } else if (agregarAMiPlaylist.innerHTML == "Agregado a tu playlist"){ //Si ya está en la playlist
-        agregarAMiPlaylist.innerText = "Agregar a mi playlist" 
-
-        let indiceCanción = cancionesFavoritas.indexOf(nombreCancion.innerHTML) //Te da el índice de la canción que le pases en el array
+    if (cancionesFavoritas.includes(id)) { //si el id está en el cancionesFavoritas
+        let indiceCanción = cancionesFavoritas.indexOf(id) //Te da el índice de la canción que le pases en el array
         cancionesFavoritas.splice(indiceCanción, 1) //Elimina la canción correspondiente a ese índice del array obtenido anteriormente
-        let cancionesFavoritasString = JSON.stringify(cancionesFavoritas) //transforma al array a stringyfy
-        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString) //vuelve a subir el array(stringyfy) al local storage
+        agregarAMiPlaylist.innerText = "Agregar de tu playlist" 
+
+    } else { //si el id no está en el cancionesFavoritas
+        cancionesFavoritas.push(id) //Agrega el id al array
+        agregarAMiPlaylist.innerText = "Quitar de tu playlist"
+
+
     }
+
+    let cancionesFavoritasString = JSON.stringify(cancionesFavoritas) //transforma al array a stringyfy
+        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString) //vuelve a subir el array(stringyfy) al local storage
 })
 
