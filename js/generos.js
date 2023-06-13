6e1a4d87bf577ab355d3cd5d8176768b6230218e
OriCacsire <ocacsiregeronimo@udesa.edu.ar>
2023-06-13 13:37:01 -0300

    se borro el enlase con index.html de generos, es por esto que no aparecia el contenido de generos.

--- js/generos.js
+++ js/generos.js
@@ -1,42 +1,49 @@
-/******************** FORMULARIO ******************************/
-let form = document.querySelector("#buscadorForm");
-let campoBuscador = document.querySelector("#searchInput");
-    form.addEventListener ("submit", function(e){
-        e.preventDefault();
-        console.log(campoBuscador.value)
-        if(campoBuscador.value == ''){
-            alert('Debe ingresar alguna palabra');
-        } else if (campoBuscador.value.length < 3){
-            alert('Ingresar al menos 3 caracteres');
-            console.log(campoBuscador.value)
-        } else{
-            this.submit();
-        }
-    })
+
+ //Declaro varables para el form 
+ let form = document.querySelector("#buscadorForm");//capturo el formulario
+ let campoBuscador = document.querySelector("#searchInput");//[name=busqueda]capturamos el campo del busqueda
+ 
+ //Agregamos un evento al buscador para que no deje buscar con campo vacio o menos de 3 carácteres
+ form.addEventListener ("submit", function(e){
+     e.preventDefault();
+     console.log(campoBuscador.value);
+     if(campoBuscador.value == ''){
+          alert('Debe ingresar alguna palabra');
+     } else if (campoBuscador.value.length < 3){
+          alert('Ingresar al menos 3 caracteres');
+     } else{
+          this.submit();
+     }
+ })
 
 /************************ GENEROS ***********************************/ 
  
- let urlgeneros = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre`
- let aCanciones = document.querySelector(".aCanciones")
-
+ let urlgeneros = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre`;
+ let listaGeneros = document.querySelector(".listaGeneros")
+ 
 fetch(urlgeneros)
     .then(function (response) {
         return response.json();
     })
+
     .then(function (data) {
         console.log(data);
+
+        let generos = '';
+
         for (let i = 0; i < data.data.length; i++) {
             console.log(data.data[i].name)
-            aCanciones.innerHTML += `<li class="listaGenero">
-                                        <a class="aclickeo" href="./detallesGenereos.html?idGenero=${data.data[i].id}">${data.data[i].name}</a>
+
+            listaGeneros.innerHTML += `<li class="listaGenero">
+                                        <a class="aclickeo" href="./detallesGeneros.html?idGenero=${data.data[i].id}">${data.data[i].name}</a>
                                     </li>`;
         
         
         }
-        return data; 
 }
 )
 .catch(function (error) {
     return error;
-}
-)
+ });
+
+
